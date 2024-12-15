5dffcb92724679030dca5dc70820d5b8b4bb0e35
Hrithic <hrithicraj730@gmail.com>
2024-12-16 03:42:03 +0530

    routes structure changed, app.jsx updated, protucted routed added

---
+++ src/admin/routes/AdminProtectedRoutes.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Outlet, Navigate } from 'react-router-dom';
+
+const AdminProtectedRoutes = () => {
+  const isAuthenticated = true;
+
+  return isAuthenticated ? <Outlet /> : <Navigate to="/" />;
+};
+
+export default AdminProtectedRoutes;    
---
+++ src/admin/routes/AdminRoutes.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import Dashboard from '../pages/Dashboard'
+
+const AdminRoutes = [
+  {path: '/admin', element:<Dashboard/>}
+]
+
+export default AdminRoutes
