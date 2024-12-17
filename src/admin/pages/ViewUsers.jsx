79a8f5bedb55e29bc305e951d06e88fdf62dc4b1
AbhayPc <abhaypc112@gmail.com>
2024-12-17 15:30:32 +0530

    Admin view-orders functionality completed

--- src/admin/pages/ViewUsers.jsx
+++ src/admin/pages/ViewUsers.jsx
@@ -1,6 +1,20 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { getAllUsers, updateUser } from '../../api/adminApi';
 
 function ViewUsers() {
+  const [users,setUsers] = useState([]);
+  useEffect(()=>{
+    getAllUsers()
+    .then((res) => setUsers(res.data));
+  },[])
+
+  function handleOnClick (userId,status) {
+    updateUser(userId,{block:!status})
+    .then(()=>{
+      getAllUsers()
+      .then((res) => setUsers(res.data));
+    })
+  }
   return (
     <div>
       <section>
@@ -10,16 +24,27 @@ function ViewUsers() {
                         <th>NAME</th>
                         <th>USERNAME</th>
                         <th>EMAIL</th>
+                        <th>ROLE</th>
                         <th>ORDERS</th>
                         <th>BLOCK</th>
                     </tr>
-                    <tr className='text-center border-y-2 h-14 text-xs md:text-base'>
-                        <td>Abhay pc</td>
-                        <td>abhay</td>
-                        <td>abhay@gmail.com</td>
-                        <td>11</td>
-                        <td><button className='bg-yellow-400 px-2 rounded-md p-1'>Block</button></td>
-                    </tr>
+                    {
+                      users && users.map((user,index)=>{
+                        if (user.role !== 'admin'){
+
+                          return(
+                          <tr key={index} className='text-center border-y-2 h-14 text-xs md:text-base'>
+                              <td>{user.name}</td>
+                              <td>{user.username}</td>
+                              <td>{user.email}</td>
+                              <td>{user.role}</td>
+                              <td>11</td>
+                              <td className='w-20'><button onClick={()=>handleOnClick(user.id,user.block)} className='bg-yellow-400 px-2 rounded-md p-1'>{user.block?"Unblock":"Block"}</button></td>
+                        </tr>
+                          )
+                        }
+                      })
+                    }
                     
                     
                 </table>
